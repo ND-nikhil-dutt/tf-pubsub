a9d2eeb7ab7d28fa1d0ac55c049a21ba82b59308
ND-nikhil-dutt <103510222+ND-nikhil-dutt@users.noreply.github.com>
2022-04-12 20:05:23 +0530

    create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+
+resource "google_storage_bucket" "storage" {
+  name = var.bucket_name
+  project = var.project_id
+  location = var.bucket_location
+}
+
+resource "google_pubsub_topic" "pubsub" {
+  name = var.topic_name
+  project = var.project_id
+  
+}
+
+resource "google_storage_notification" "notification" {
+  bucket         = var.bucket_name
+  payload_format = "JSON_API_V1"
+  topic          = var.topic_name
+  event_types    = [var.event_type]
+  depends_on = [google_pubsub_topic_iam_binding.binding]
+}
+
+resource "google_pubsub_topic_iam_binding" "binding" {
+  members = ["serviceAccount:tf-gke-gke-test-1-6fwu@nik-learning.iam.gserviceaccount.com"]
+  role    = "roles/pubsub.publisher"
+  topic   = google_pubsub_topic.pubsub.id
+}
+
+resource "google_pubsub_subscription" "echo" {
+  name  = "echo"
+  topic = google_pubsub_topic.pubsub.name
+}
